b09c450fefba30039deadbc0846cfb8bf4578c04
Sean Colsen <colsen.sean@gmail.com>
2022-04-26 11:46:22 -0400

    Add SelectColumns component

---
+++ mathesar_ui/src/components/SelectColumns.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { Column } from '@mathesar/stores/table-data/columns';
+  import { MultiSelect } from '@mathesar-component-library';
+  import type { ComponentAndProps } from '@mathesar-component-library/types';
+  import ColumnName from './ColumnName.svelte';
+
+  export let availableColumns: Column[];
+  export let columns: Column[] = [];
+
+  function getLabel(column: Column): ComponentAndProps {
+    return {
+      component: ColumnName,
+      props: { column },
+    };
+  }
+</script>
+
+<MultiSelect options={availableColumns} bind:values={columns} {getLabel} />
